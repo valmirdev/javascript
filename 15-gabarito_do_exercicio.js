9e7c9c726ee4ccacc81ba186112bdfb859b558b8
valmirdev <valmir473355@gmail.com>
2024-12-29 22:01:32 -0300

    four commit

---
+++ 15-gabarito_do_exercicio.js
@@ -0,0 +1,44 @@
+// Exercício 1: Escreva um programa simples que exiba uma string no terminal utilizando
+// o console.log.
+console.log("Olá, mundo!");
+
+// Exercício 2: Modifique o programa anterior para armazenar a mensagem em uma
+// variável antes de exibi-la no console.
+let mensagem = "Olá, mundo!";
+console.log(mensagem);
+
+// Exercício 3: Declare três variáveis mutáveis: nome (string), idade (number) e
+//isStudent (boolean).
+let nome = "Alice";
+let idade = 28;
+let isStudent = true;
+
+// Exercício 4: Modifique o valor das variáveis anteriores e imprima os novos valores no
+//console.
+nome = "Paulo";
+idade = 34;
+isStudent = false;
+console.log(nome, idade, isStudent);
+
+// Exercício 5: Crie uma variável constante que irá armazenar um número e imprima no
+//console.
+const NUMBER_NEEDED = 1001;
+console.log(NUMBER_NEEDED);
+
+//  Exercício 6: Declare duas variáveis, endereco e telefone, sem atribuir valores a
+//elas.
+let endereco;
+let telefone;
+
+// Exercício 7: Declare variáveis com nomes descritivos para armazenar o nome de um
+//produto, seu preço e a quantidade em estoque.
+let nomeProduto = "Notebook";
+let precoProduto = 1500;
+let quantidadeEmEstoque = 20;
+console.log(nomeProduto, precoProduto, quantidadeEmEstoque);
+
+// Exercício 8: Declare uma variável para armazenar o nome de uma cidade e outra para
+//armazenar a sua população. Exiba uma mensagem combinando ambas as variáveis.
+let cidade = "São Paulo";
+let populacao = 12300000;
+console.log(`A cidade de ${cidade} tem uma população de ${populacao} habitantes.`);
