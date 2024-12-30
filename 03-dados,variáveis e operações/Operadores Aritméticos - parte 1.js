49e2ddced4e3daeb7812517312028586f78758a6
valmirdev <valmir473355@gmail.com>
2024-12-30 00:31:57 -0300

    first commit

---
+++ 03-dados,variáveis e operações/Operadores Aritméticos - parte 1.js
@@ -0,0 +1,25 @@
+//Adição
+let valor = 10;
+let segundoValor = 43;
+
+console.log(valor + segundoValor);
+
+// let mensagem = "20";
+//console.log(mensagem + valor); // concatenação
+
+//Subtração
+console.log(valor - segundoValor);
+console.log(segundoValor - valor);
+
+//Divisão
+console.log(valor / segundoValor);
+console.log(segundoValor / valor);
+
+// Módulo
+console.log(valor % segundoValor);
+console.log(segundoValor % valor);
+
+let modulo = 10 % 2;
+let divisao = 10 / 2;
+console.log(modulo);
+console.log(divisao);
---
+++ 03-dados,variáveis e operações/Operadores Aritméticos - parte 2.js
@@ -0,0 +1,16 @@
+//Adição + Subtração - Multiplicação * Divisão / Módulo % Inversão de sinal/negativo -
+
+// Operadores Aritméticos avançados
+//exponenciação **
+let base = 2;
+let expoente = 3;
+let resultado = base ** expoente; // 2 seja elevado a 3
+console.log(resultado);
+
+//Incremento ++ (adicionar o valor 1) Decremento -- (diminuir o valor 1)
+let controle = 0;
+// console.log(controle++);
+console.log(controle--);
+console.log(controle);
+console.log(controle--);
+console.log(controle);
